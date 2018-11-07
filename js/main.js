2afc4d04a2f85a83cf9812f9bd1428df9af4bbc4
Brendan Rogers <brendanr11@gmail.com>
2018-11-07 12:10:36 -0500

    Installed waypoints and animate.css. Requires further implementation.

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@
 
 let years = document.querySelectorAll(".data-ref");
 
+var waypoint = new Waypoint({
+  element: document.getElementById('thing'),
+  handler: function(direction) {
+    
+  }
+});
+
 function getData() {
 	// we set up the URL with our Superglobal variable, which will be grabbed by our PHP to serve us content
 
-
-
 	let targetURL = `./includes/connect.php?year=${this.id}`;  
     // THIS LINE OF CODE
-
-
 
 	fetch(targetURL) //go get the data and bring it back
     .then(res => res.json()) //Turn the result into a plain JS object
@@ -27,12 +30,10 @@ function getData() {
 // moves the data to the actual website
 function showYearData(data) {
 	const { year, description } = data;
-
     // The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
 
     document.getElementById('tlHead').innerHTML = `${year}`;
     document.getElementById('tlDesc').innerHTML = `${description}`;
-
 }
 
 years.forEach( year =>  year.addEventListener('mouseover', getData));
